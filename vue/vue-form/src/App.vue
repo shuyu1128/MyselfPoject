5bb8284121556555168fa5c554e5490c8f550016
shuyu1128 <47551175+shuyu1128@users.noreply.github.com>
2020-11-19 17:01:25 +0800

    2020.11.19

---
+++ vue/vue-form/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <el-form label-width="100px">
+      <el-form-item label="活动名称">
+        <el-input></el-input>
+      </el-form-item >
+      <el-form-item label="活动区域">
+        <el-select placeholder="活动区域选择">
+          <el-option label="区域一"></el-option>
+          <el-option label="区域二"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="活动时间">
+        <el-col>
+          <el-form-item>
+            <el-date-picker placeholder="选择日期"></el-date-picker>
+          </el-form-item>
+        </el-col>
+        <el-col>
+          el-form-item
+        </el-col>
+      </el-form-item>
+    </el-form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
